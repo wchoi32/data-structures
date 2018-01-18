1ac00fa146ecafb826b68e508ce54fce1f977215
WooSung Choi <johnwschoi@gmail.com>
2018-01-18 06:45:29 -0800

    Finished up to ES6. Part 1 Complete

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,26 @@
 class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  constructor() {
+  constructor(enq, deq) {
+  	this.enq = 0;
+  	this.deq = 0;
   }
+  enqueue (value) {
+    this[this.enq] = value;
+    this.enq++;
+  };
+
+  dequeue () {
+    if(this.enq - this.deq) {
+      var beforeDelete = this[this.deq];
+      delete this[this.deq];
+      this.deq++
+      return beforeDelete;
+    }
+  };
+
+  size () {
+    return this.enq - this.deq;
+  };
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,25 +1,25 @@
 class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  constructor() {
-  	this.count = this;
+  constructor(count) {
+  	this.count = 0;
   }
 
-  canEditEverything() { 
-  	Stack.push = function(value) {
-		this[this.count] = value;
-		this.count += 1;
-	};
-	Stack.pop = function() {
-		this.count -= 1;
-		return this[this.count];
-	};
-	Stack.size = function() {
-		if (this.count < 0) {
-			return 0;
-		}
-		return this.count;
-	};
+  push (value) {
+	this[this.count] = value;
+	this.count += 1;
+  }
+
+  pop () {
+	this.count -= 1;
+	return this[this.count];
+  };
+
+  size () {
+	if (this.count < 0) {
+		return 0;
+	}
+	return this.count;
   }
 }
 
